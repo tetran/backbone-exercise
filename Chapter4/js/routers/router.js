bf1091ac2fc59994ef136e228ecdfa434eb70605
tetran <coarse.ground@gmail.com>
2014-02-11 01:50:51 +0900

    finished Chapter 4

--- Chapter4/js/routers/router.js
+++ Chapter4/js/routers/router.js
@@ -1,3 +1,17 @@
 /**
  * Created by tetran on 2014/02/09.
  */
+var Workspace = Backbone.Router.extend({
+    routes: {
+        '*filter': 'setFilter'
+    },
+
+    setFilter: function(param) {
+        app.TodoFilter = param || '';
+
+        window.app.Todos.trigger('filter');
+    }
+});
+
+app.TodoRouter = new Workspace();
+Backbone.history.start();
--- Chapter4/js/views/todos.js
+++ Chapter4/js/views/todos.js
@@ -10,23 +10,47 @@ app.TodoView = Backbone.View.extend({
     template: _.template($('#item-template').html()),
 
     events: {
+        'click .toggle': 'togglecompleted',
         'dblclick label': 'edit',
+        'click .destroy': 'clear',
         'keypress .edit': 'updateOnEnter',
         'blur .edit': 'close'
     },
 
     initialize: function() {
         this.listenTo(this.model, 'change', this.render);
+        this.listenTo(this.model, 'destroy', this.remove);
+        this.listenTo(this.model, 'visible', this.toggleVisible);
     },
 
     render: function() {
         this.$el.html(this.template(this.model.toJSON()));
+
+        this.$el.toggleClass('completed', this.model.get('completed'));
+        this.toggleVisible();
+
         this.$input = this.$('.edit');
         return this;
     },
 
+    toggleVisible: function() {
+        this.$el.toggleClass('hidden', this.isHidden());
+    },
+
+    isHidden: function() {
+        var isCompleted = this.model.get('completed');
+        return (
+            (!isCompleted && app.TodoFilter === 'completed')
+            || (isCompleted && app.TodoFilter === 'active')
+        );
+    },
+
+    togglecompleted: function() {
+        this.model.toggle();
+    },
+
     edit: function() {
-        this.$el.addClass( 'editing');
+        this.$el.addClass('editing');
         this.$input.focus();
     },
 
@@ -35,6 +59,8 @@ app.TodoView = Backbone.View.extend({
 
         if (value) {
             this.model.save({title: value});
+        } else {
+            this.clear();
         }
 
         this.$el.removeClass('editing');
@@ -44,5 +70,9 @@ app.TodoView = Backbone.View.extend({
         if (e.which === ENTER_KEY) {
             this.close();
         }
+    },
+
+    clear: function() {
+        this.model.destroy();
     }
 });
